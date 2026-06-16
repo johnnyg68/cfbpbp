c3eb968dd504b63082a324baae5ca0349c234353
jgilliam-magento <gilliam@adobe.com>
2026-06-16 14:32:41 -0500

    make result local variable not global. prevent race condition

--- src/main/java/com/jmg/spring/cfbpbp/player/PlayerRepository.java
+++ src/main/java/com/jmg/spring/cfbpbp/player/PlayerRepository.java
@@ -17,19 +17,20 @@ public class PlayerRepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	ColumnMapRowMapper mapper = new ColumnMapRowMapper();
 	ObjectMapper om = new ObjectMapper();
-	List<Map<String, Object>> result;
-	
-	// Generic SQL driven player stat categories for a given year	
+	// List<Map<String, Object>> result; // safeguard against race condition. make
+	// this a local variable instead of a class variable
+
+	// Generic SQL driven player stat categories for a given year
 	public String getPlayerStats(String year, String sqlFile) throws IOException {
 		String sql = SqlFileReader.getSqlFromFile(sqlFile);
-		result = jdbcTemplate.query(sql, mapper, year);
+		List<Map<String, Object>> result = jdbcTemplate.query(sql, mapper, year);
 		String json = om.writeValueAsString(result);
 		return json;
 	}
-	
+
 	// Career Stats grouped by year
 	public String getPlayerCareerPassing(String playerId) throws IOException {
 		String sqlFile = "player/player_career_passing.sql";
@@ -56,7 +57,7 @@ public class PlayerRepository {
 		return resultAsJson(sqlFile, playerId);
 	}
 
-	public String getPlayerCareerKicking(String playerId) throws IOException{
+	public String getPlayerCareerKicking(String playerId) throws IOException {
 		String sqlFile = "player/player_career_kicking.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
@@ -70,69 +71,70 @@ public class PlayerRepository {
 		String sqlFile = "player/player_career_kickreturns.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	// Career Game Stats - Game Log
 	public String getPlayerGamesPassing(String playerId) throws IOException {
 		String sqlFile = "player/player_games_passing.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesRushing(String playerId) throws IOException {
 		String sqlFile = "player/player_games_rushing.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesReceiving(String playerId) throws IOException {
 		String sqlFile = "player/player_games_receiving.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesDefense(String playerId) throws IOException {
 		String sqlFile = "player/player_games_defense.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesKickReturns(String playerId) throws IOException {
 		String sqlFile = "player/player_games_kickreturns.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesPuntReturns(String playerId) throws IOException {
 		String sqlFile = "player/player_games_puntreturns.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesPunting(String playerId) throws IOException {
 		String sqlFile = "player/player_games_punting.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerGamesKicking(String playerId) throws IOException {
 		String sqlFile = "player/player_games_kicking.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
-// Career totals - aggregated over all years, i.e. not by year but all time stats
+
+	// Career totals - aggregated over all years, i.e. not by year but all time
+	// stats
 	public String getPlayerCareerTotalPassing(String playerId) throws IOException {
 		String sqlFile = "player/player_career_total_passing.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerCareerTotalRushing(String playerId) throws IOException {
 		String sqlFile = "player/player_career_total_rushing.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerCareerTotalReceiving(String playerId) throws IOException {
 		String sqlFile = "player/player_career_total_receiving.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerCareerTotalDefense(String playerId) throws IOException {
 		String sqlFile = "player/player_career_total_defense.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-	
+
 	public String getPlayerCareerTotalKickReturns(String playerId) throws IOException {
 		String sqlFile = "player/player_career_total_kickreturns.sql";
 		return resultAsJson(sqlFile, playerId);
@@ -152,10 +154,11 @@ public class PlayerRepository {
 		String sqlFile = "player/player_career_total_kicking.sql";
 		return resultAsJson(sqlFile, playerId);
 	}
-// UTILITY	
+
+	// UTILITY
 	private String resultAsJson(String sqlFile, String playerId) throws IOException {
 		String sql = SqlFileReader.getSqlFromFile(sqlFile);
-		result = jdbcTemplate.query(sql, mapper, playerId);                
+		List<Map<String, Object>> result = jdbcTemplate.query(sql, mapper, playerId);
 		String json = om.writeValueAsString(result);
 		return json;
 
